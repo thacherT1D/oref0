1a1063d4ba871d3a639289bd7905bdfb1925d904
Scott <scottleibrand@gmail.com>
2017-07-16 12:02:50 -0700

    clone oref0 install-scripts branch and run openaps-packages.sh from there

--- bin/openaps-install.sh
+++ bin/openaps-install.sh
@@ -14,19 +14,25 @@ passwd -S edison | grep 20[01][0-6] && passwd -e edison -i 3
 # set timezone
 dpkg-reconfigure tzdata
 
+# TODO: remove this after Debian's IPv6 mirrors are stable again
+mkdir -p /etc/apt/apt.conf.d/
+echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4
+
 #dpkg -P nodejs nodejs-dev
-# TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
-apt-get -o Acquire::ForceIPv4=true update && apt-get -o Acquire::ForceIPv4=true -y dist-upgrade && apt-get -o Acquire::ForceIPv4=true -y autoremove
-apt-get -o Acquire::ForceIPv4=true install -y sudo strace tcpdump screen acpid vim python-pip locate
+apt-get update && apt-get -y dist-upgrade && apt-get -y autoremove
+apt-get install -y sudo strace tcpdump screen acpid vim python-pip locate
 adduser edison sudo
 adduser edison dialout
 
 sed -i "s/daily/hourly/g" /etc/logrotate.conf
 sed -i "s/#compress/compress/g" /etc/logrotate.conf
 
-# TODO: change back to master after docs release
-curl -s https://raw.githubusercontent.com/openaps/docs/dev/scripts/quick-packages.sh | bash -
-mkdir -p ~/src; cd ~/src && git clone git://github.com/openaps/oref0.git || (cd oref0 && git checkout master && git pull)
-echo "Press Enter to run oref0-setup with the current release (master branch) of oref0,"
+#curl -s https://raw.githubusercontent.com/openaps/oref0/install-scripts/bin/openaps-packages.sh | bash -
+
+# TODO: change back to dev after merging install-scripts to dev, then to master after next oref0 release
+mkdir -p ~/src; cd ~/src && git clone -b install-scripts git://github.com/openaps/oref0.git || (cd oref0 && git checkout install-scripts && git pull)
+~/src/oref0/bin/openaps-packages.sh
+#echo "Press Enter to run oref0-setup with the current release (master branch) of oref0,"
+echo "Press Enter to run oref0-setup with the install-scripts branch of oref0,"
 read -p "or press ctrl-c to cancel. " -r
 cd && ~/src/oref0/bin/oref0-setup.sh
